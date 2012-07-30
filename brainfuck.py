21446cd1db34f1bcec5eeaac8e246103bbd21f87
Nick Platt <platt.nicholas@gmail.com>
2012-07-30 00:39:00 -0400

    Stomped bug in start_loop. Better input support. Added Julython web hook.

--- brainfuck.py
+++ brainfuck.py
@@ -1,10 +1,13 @@
 #! /usr/bin/python3
+import time
 
 class Brainfuck:
 
-    debug   = False
+    debug = False
+    out_string = ""
+    in_string = ""
 
-    def __init__(self, cells=5):
+    def __init__(self, cells=20):
 
         self.memory  = [0 for x in range(0, cells)]
         self.pointer = 0
@@ -34,6 +37,8 @@ class Brainfuck:
         self.program = program
         print (self.program)
         while self.cursor < len(self.program):
+            #print ("\r{0}^".format(" " * self.cursor), end="")
+
             if self.debug:
                 print ("Token: {0}  {1}".format(self.program[self.cursor], [m for m in self.memory]), end="")
 
@@ -43,19 +48,24 @@ class Brainfuck:
                 print (" -> {0}".format([m for m in self.memory]))
 
             self.cursor += 1
+            #time.sleep(.01)
+
+        if self.out_string:
+            print(self.out_string)
+
 
     ## Operators
     def forward(self):
         if self.pointer < len(self.memory)  - 1:
             self.pointer += 1
         else:
-            exit("Fatal Error: Index out of range")
+            exit("Fatal Error: Index out of range (high)")
 
     def backward(self):
         if self.pointer > 0:
             self.pointer -= 1
         else:
-            exit("Fatal Error: Index out of range")
+            exit("Fatal Error: Index out of range (low)")
 
     def increment(self):
         self.memory[self.pointer] += 1
@@ -64,36 +74,57 @@ class Brainfuck:
         self.memory[self.pointer] -= 1
 
     def output(self):
-        print(chr(self.memory[self.pointer]), end="")
+        self.out_string += chr(self.memory[self.pointer])
+        #print(chr(self.memory[self.pointer]), end="")
 
     def input(self):
-        try: self.syntax[input()]()
-        except KeyError: pass
+        while not self.in_string:
+            self.in_string = input()
+            if self.in_string == "":
+                break
+
+        h = self.in_string[0:1]
+
+        if h == "":
+            self.memory[self.pointer] = -1
+        else:
+            self.in_string = self.in_string[1:]
+            self.memory[self.pointer] = ord(h)
 
     def start_loop(self):
         if self.memory[self.pointer] == 0:
-            try: self.cursor = self.program.index("]", self.cursor)
-            except ValueError: exit("Fatal Error: Unmatched '[' at position {0}".format(self.cursor))
-
-    def stop_loop(self):
-        bracket = 0
-        if self.memory[self.pointer] != 0:
-            for c in range(self.cursor-1, -1, -1):
+            brackets = 0
+            for c in range(self.cursor+1, len(self.program)):
                 t = self.program[c]
-                if t == ']':
-                    bracket += 1
-                elif t == '[':
-                    if bracket > 0:
-                        bracket -= 1
+                if t == '[':
+                    brackets += 1
+                elif t == ']':
+                    if brackets > 0:
+                        brackets -= 1
                     else:
                         self.cursor = c
                         return
 
-            if bracket > 0:
-                exit("Fatal Error: Unmatched ']' at position {0}".format(self.cursor))
+            exit("Fatal Error: Unmatched '[' at position {0}".format(self.cursor))
+
+    def stop_loop(self):
+        if self.memory[self.pointer] != 0:
+            brackets = 0
+            for c in range(self.cursor-1, -1, -1):
+                t = self.program[c]
+                if t == ']':
+                    brackets += 1
+                elif t == '[':
+                    if brackets > 0:
+                        brackets -= 1
+                    else:
+                        self.cursor = c
+                        return
+
+            exit("Fatal Error: Unmatched ']' at position {0}".format(self.cursor))
 
 
 if __name__ == '__main__':
     bf = Brainfuck()
-    #bf.run("+++[>+<-]>>+")
-    bf.run("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.")
+    #bf.run("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.")
+    bf.run("-,+[-[>>++++[>++++++++<-]<+<-[>+>+>-[>>>]<[[>+<-]>>+>]<<<<<-]]>>>[-]+>--[-[<->[-]]]<[++++++++++++<[>-[>+>>]>[+[<+>-]>+>>]<<<<<-]>>[<+>-]>[-[-<<[-]>>]<<[<<->>-]>>]<<[<<+>>-]]<[-]<.[-]<-,+]")
